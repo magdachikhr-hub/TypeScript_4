b6b5edfb52eb62d22fb2ebb40ef9e901187a6cc2
magda chikhradz <magda292005@gmail.com>
2026-06-25 17:15:21 +0400

    encapsulation

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ class Animal {
 }
 const newAnimal = new Animal();
 console.log(newAnimal.isMammal());
-//inheritance
+//inheritance // oop
 class Dog extends Animal {
     bark;
     constructor() {
@@ -25,4 +25,26 @@ class Dog extends Animal {
 const dog = new Dog();
 console.log(dog.isMammal());
 console.log(dog.isBarking());
+// encapsulation
+class Person {
+    #idNumber;
+    name;
+    constructor(id, name) {
+        this.#idNumber = id;
+        this.name = name;
+    }
+    isAllowed() {
+        if (this.#idNumber.length < 6) {
+            return false;
+        }
+        return true;
+    }
+}
+const person1 = new Person("5449493", "magda");
+console.log(person1);
+console.log(person1.isAllowed());
+// console.log(person1.#idNumber);
+//# makes it private
+//Property '#idNumber' is not accessible outside
+//class 'Person' because it has a private identifier.
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -16,22 +16,48 @@ class Animal {
 const newAnimal: Animal = new Animal();
 console.log(newAnimal.isMammal());
 
-//inheritance
+//inheritance // oop
 
 class Dog extends Animal {
-  bark;
+  bark: string;
   constructor() {
     super();
     this.bark = "woof";
   }
 
-  isBarking() {
+  isBarking(): string {
     return `this dog said ${this.bark}`;
   }
 }
 
-const dog = new Dog();
+const dog: Dog = new Dog();
 console.log(dog.isMammal());
 console.log(dog.isBarking());
 
+// encapsulation
 
+class Person {
+  #idNumber: string;
+  name: string;
+
+  constructor(id: string, name: string) {
+    this.#idNumber = id;
+    this.name = name;
+  }
+
+  isAllowed() {
+    if (this.#idNumber.length < 6) {
+      return false;
+    }
+    return true;
+  }
+}
+
+const person1: Person = new Person("5449493", "magda");
+console.log(person1);
+console.log(person1.isAllowed());
+// console.log(person1.#idNumber);
+
+//# makes it private
+//Property '#idNumber' is not accessible outside
+//class 'Person' because it has a private identifier.
